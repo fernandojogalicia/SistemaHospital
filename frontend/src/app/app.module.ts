77d30934511d678da86f83d0522d6829d1710c56
fernandojogalicia <fernandojo.galicia@gmail.com>
2020-11-23 17:04:52 -0600

    Creación de Front end y primeros componentes

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PacienteComponent } from './paciente/paciente.component';
+import { CasoComponent } from './caso/caso.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PacienteComponent,
+    CasoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
